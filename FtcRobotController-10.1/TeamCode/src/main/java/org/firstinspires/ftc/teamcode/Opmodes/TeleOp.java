6048ff72744bd1d7f7a4d35d1f78036c8886c054
michikira <132859997+michikira@users.noreply.github.com>
2024-11-04 16:41:26 -0800

    Update TeleOp.java

--- FtcRobotController-10.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/TeleOp.java
+++ FtcRobotController-10.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/TeleOp.java
@@ -2,7 +2,13 @@ package org.firstinspires.ftc.teamcode.Opmodes;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Drivebase;
 
 public class TeleOp extends LinearOpMode{
     @Override
@@ -16,5 +22,32 @@ public class TeleOp extends LinearOpMode{
         }
         CommandScheduler.getInstance().reset();
     }
+
+    @com.qualcomm.robotcore.eventloop.opmode.TeleOp
+    public class TeleOP extends CommandOpMode {
+
+        private GamepadEx driverOP;
+        private GamepadEx buttonOP;
+        private Drivebase drivebase;
+
+        @Override
+        public void initialize() {
+            driverOP = new GamepadEx(gamepad1);
+            buttonOP = new GamepadEx(gamepad2);
+
+            drivebase = new Drivebase(hardwareMap);
+            GamepadButton armUp = new GamepadButton(buttonOP, GamepadKeys.Button.X);
+            GamepadButton armDown = new GamepadButton(buttonOP, GamepadKeys.Button.Y);
+            GamepadButton gripUp = new GamepadButton(buttonOP, GamepadKeys.Button.B);
+            GamepadButton gripDown = new GamepadButton(buttonOP, GamepadKeys.Button.A);
+
+
+            drivebase.setDefaultCommand(new RunCommand(() -> drivebase.driveRobotCentric(driverOP.getLeftY(), driverOP.getLeftX(), driverOP.getRightX()), drivebase));
+
+
+        }
+
+    }
+
 
 }
